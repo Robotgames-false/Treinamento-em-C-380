4bb7daabb93fa06b96eb93f329690e633f97bde0
Mr.Robot != false <elisiomassaqui8@gmail.com>
2024-05-07 20:17:13 +0100

    nottt

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     int zero = 0;
     
-    printf("O %d not foi o primeiro numero a ser descoberto \n", zero);
+    printf("O primeiro number descoberto nottt foi o %d", zero);
   
   system("PAUSE");	
   return 0;
