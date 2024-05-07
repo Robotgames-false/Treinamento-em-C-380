cd7728d179b61984404417345ee704b0700f8057
Mr.Robot != false <elisiomassaqui8@gmail.com>
2024-05-07 20:40:25 +0100

    printf
    Para usar o comando printf de maneira eficiente em C, é importante entender como formatar a saída de acordo com suas necessidades. Aqui estão alguns exercícios com diferentes casos de uso do printf, além de alguns exemplos de códigos especiais que você pode usar para controlar a formatação.
    
    Exercício 1: Impressão de Variáveis Simples
    Imprima uma série de variáveis de diferentes tipos usando printf.

--- Special codes and printf format/main.c
+++ Special codes and printf format/main.c
@@ -3,6 +3,15 @@
 
 int main(int argc, char *argv[])
 {
+    int a = 10;
+    float b = 5.5;
+    double c = 3.141592;
+    char d = 'A';
+    
+    printf("Valor de 'a' (int): %d\n", a);
+    printf("Valor de 'b' (float): %.2f\n", c);
+    printf("Valor de 'c' (double): %.61f\n", c);
+    printf("Valor de 'd' (char): %c\n", d);
   
   system("PAUSE");	
   return 0;
