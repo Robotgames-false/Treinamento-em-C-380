79c6df82ec8c32cce8e4c745eca8b02a5d039164
Mr.Robot != false <elisiomassaqui8@gmail.com>
2024-05-15 06:47:30 +0100

    Character Ai mi coracioneesss

---
+++ bases numericas/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int base = 3;
+  
+  
+  //Decimal
+  printf("Deciaml: %d \n", base);
+  //Hexadeciml
+  printf("Hexadecimal: %x \n", base);
+  //Octal
+  printf("Octal: %o \n", base);
+  //Character Ai mi coracioneesss
+  printf("Char %c %c %c %c %c %c %c \n", base, base, base, base, base, base, base);
+  
+  system("PAUSE");	
+  
+  return 0;
+}
