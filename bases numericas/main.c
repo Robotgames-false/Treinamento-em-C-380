fb81e699a4ac40ab0149683d818e6e9d9756c93a
Mr.Robot != false <elisiomassaqui8@gmail.com>
2024-05-15 06:51:47 +0100

    bases

--- bases numericas/main.c
+++ bases numericas/main.c
@@ -4,16 +4,20 @@
 int main(int argc, char *argv[])
 {
   int base = 3;
+  int base01 = 65;
   
   
   //Decimal
-  printf("Deciaml: %d \n", base);
+  printf("Deciaml: %d \n", base01);
   //Hexadeciml
-  printf("Hexadecimal: %x \n", base);
+  printf("Hexadecimal: %x \n", base01);
   //Octal
-  printf("Octal: %o \n", base);
+  printf("Octal: %o \n", base01);
   //Character Ai mi coracioneesss
   printf("Char %c %c %c %c %c %c %c \n", base, base, base, base, base, base, base);
+  
+  //Char letra
+  printf("Char letra: %c \n", base01);
   
   system("PAUSE");	
   
