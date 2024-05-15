add7c5af1ea1885953c0d799b51442173610182c
Mr.Robot != false <elisiomassaqui8@gmail.com>
2024-05-15 06:25:08 +0100

    Tamanho de campo pra float a direita e ao mesmo tempo com arredondaemto

--- tamanho de campo/main.c
+++ tamanho de campo/main.c
@@ -3,16 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-    //Para decimais, tamanho de campo a esquerda
+    //Para decimais, tamanho de campo a esquerda.
     printf("%15d %16d %12d %19d \n", 20, 30, 40, 50);
     
-    //Pra de decimais tamamnho de campo a direita
+    //Pra de decimais tamamnho de campo a direita.
     printf("%-12d %-15d %-16d \n", 20, 60, 10);
     
-    //Tamanho de campo pra float a esquerda e ao mesmo tempo com arredondamento
+    //Tamanho de campo pra float a esquerda e ao mesmo tempo com arredondamento.
     printf("%.1f \t %.1f \t %1f \t \n", 49.98, 19.99, 25.66);
     
-    //Tamanho de campo pra float a direita e ao mesmo tempo com arredondaemto
+    //Tamanho de campo pra float a direita e ao mesmo tempo com arredondaemto.
     printf("%-15f %-20f %-10f \n", 39.9, 41.80, 17.17);
     
 
